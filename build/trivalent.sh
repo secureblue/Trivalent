2544b4be33cc209a06b0fad7051c9faa8e96af17
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-06-04 15:36:31 -0700

    fix

--- build/trivalent.sh
+++ build/trivalent.sh
@@ -69,9 +69,4 @@ exec < /dev/null
 exec > >(exec cat)
 exec 2> >(exec cat >&2)
 
-exec bwrap $BWRAP_ARGS $HERE/$CHROMIUM_NAME $CHROMIUM_FLAGS "$@"
-
-
-while [ true ]; do
-  echo "test"
-done
+exec bwrap $BWRAP_ARGS "$HERE/$CHROMIUM_NAME" $CHROMIUM_FLAGS "$@"
