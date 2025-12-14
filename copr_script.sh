fe9040c20ea35a226d8900c5e84ba30f07cb5da9
Rootkit404 <175176948+RKNF404@users.noreply.github.com>
2025-12-13 22:46:09 -0500

    Update copr_script.sh

--- copr_script.sh
+++ copr_script.sh
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 set -oue pipefail
+shopt -s nullglob
 
 wget https://versionhistory.googleapis.com/v1/chrome/platforms/linux/channels/stable/versions/all/releases?filter=endtime=none -O chromium-version.json
 grep \"version\" chromium-version.json | grep -oh "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" > chromium-version.txt
